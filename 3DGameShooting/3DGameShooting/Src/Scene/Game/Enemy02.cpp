be9afbd9c6a036b3f877eae1d9c5ffcf44099adf
koutano <koutano0311@gmail.com>
2023-01-12 10:35:38 +0900

    Enemy02実装

--- 3DGameShooting/3DGameShooting/Src/Scene/Game/Enemy02.cpp
+++ 3DGameShooting/3DGameShooting/Src/Scene/Game/Enemy02.cpp
@@ -1,6 +1,30 @@
 #include "Enemy02.h"
+#include "../../Library/Library.h"
+#define _USE_MATH_DEFINES
+#include <math.h>
 
 namespace Game
 {
+	Enemy02::Enemy02(VECTOR pos_) :
+		EnemyBase(pos_, VGet(1, 1, 1), VGet(0, 180, 0))
+	{
+		hp = 1000;
+	}
 
+	void Enemy02::Update()
+	{
+		pos.x += sinf(degreeAngle.y / 180 * M_PI) * speed;
+		pos.z += cosf(degreeAngle.y / 180 * M_PI) * speed;
+
+		--deleteTime;
+		if (deleteTime < 0)
+		{
+			SetDeadFlag(true);
+		}
+	}
+
+	void Enemy02::Draw()
+	{
+		TEngine::Library::DrawModel("enemy02", pos, degreeAngle, scale);
+	}
 }
--- 3DGameShooting/3DGameShooting/Src/Scene/Game/Enemy02.h
+++ 3DGameShooting/3DGameShooting/Src/Scene/Game/Enemy02.h
@@ -1,11 +1,19 @@
 #ifndef ENEMY_02_H_
 #define ENEMY_02_H_
 
+#include "EnemyBase.h"
+#include <DxLib.h>
+
 namespace Game
 {
-	class Enemy02
+	class Enemy02:public EnemyBase
 	{
-
+	public:
+		Enemy02(VECTOR pos_);
+		~Enemy02()override = default;
+	public:
+		void Update()override;
+		void Draw()override;
 	};
 }
 
