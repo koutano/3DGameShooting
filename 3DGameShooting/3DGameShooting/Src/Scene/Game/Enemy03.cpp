a4848043583d2c0d396a3dac6954ac38804aedaf
koutano <koutano0311@gmail.com>
2023-01-13 15:24:39 +0900

    Enemy03の追加

---
+++ 3DGameShooting/3DGameShooting/Src/Scene/Game/Enemy03.cpp
@@ -0,0 +1,30 @@
+﻿#include "Enemy03.h"
+#include "../../Library/Library.h"
+#define _USE_MATH_DEFINES
+#include <math.h>
+
+namespace Game
+{
+	Enemy03::Enemy03(VECTOR pos_) :
+		EnemyBase(pos_,VGet(1,1,1),VGet(0,180,0))
+	{
+	}
+
+	void Enemy03::Update()
+	{
+		pos.x += sinf(degreeAngle.y / 180 * M_PI) * speed;
+		pos.z += cosf(degreeAngle.y / 180 * M_PI) * speed;
+
+		--deleteTime;
+		if (deleteTime < 0)
+		{
+			SetDeadFlag(true);
+		}
+	}
+
+	void Enemy03::Draw()
+	{
+		TEngine::Library::DrawModel("enemy03", pos, degreeAngle, scale);
+	}
+
+}
---
+++ 3DGameShooting/3DGameShooting/Src/Scene/Game/Enemy03.h
@@ -0,0 +1,20 @@
+﻿#ifndef ENEMY_03_H_
+#define ENEMY_03_H_
+
+#include "EnemyBase.h"
+#include "DxLib.h"
+
+namespace Game
+{
+	class Enemy03:public EnemyBase
+	{
+	public:
+		Enemy03(VECTOR pos_);
+		~Enemy03()override = default;
+	public:
+		void Update()override;
+		void Draw()override;
+	};
+}
+
+#endif // !ENEMY_03_H_
