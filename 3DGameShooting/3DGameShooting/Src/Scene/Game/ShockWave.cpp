468b6b6c3ece6ac62685c5005bac1f31f1196453
koutano <koutano0311@gmail.com>
2023-01-13 16:04:17 +0900

    衝撃波クラスの作成

---
+++ 3DGameShooting/3DGameShooting/Src/Scene/Game/ShockWave.cpp
@@ -0,0 +1,23 @@
+﻿#include "ShockWave.h"
+#include "../../Library/Library.h"
+#define _USE_MATH_DEFINES
+#include <math.h>
+
+namespace Game
+{
+	ShockWave::ShockWave(VECTOR pos_) :
+		ObjBase(pos_,VGet(1,1,1),VGet(0,180,0))
+	{
+	}
+
+	void ShockWave::Update()
+	{
+		
+	}
+
+	void ShockWave::Draw()
+	{
+		TEngine::Library::DrawModel("enemy03", pos, degreeAngle, scale);
+	}
+
+}
---
+++ 3DGameShooting/3DGameShooting/Src/Scene/Game/ShockWave.h
@@ -0,0 +1,20 @@
+﻿#ifndef SHOCK_WAVE_H_
+#define SHOCK_WAVE_H_
+
+#include "DxLib.h"
+#include "ObjBase.h"
+
+namespace Game
+{
+	class ShockWave:public ObjBase
+	{
+	public:
+		ShockWave(VECTOR pos_);
+		~ShockWave()override = default;
+	public:
+		void Update()override;
+		void Draw()override;
+	};
+}
+
+#endif // !ENEMY_03_H_
