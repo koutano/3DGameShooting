6543d93e95c63515f10143da97ff993290d6c854
koutano <koutano0311@gmail.com>
2023-01-15 13:36:27 +0900

    ShockWave管理クラスの作成

---
+++ 3DGameShooting/3DGameShooting/Src/Scene/Game/ShockWaveManager.cpp
@@ -0,0 +1,54 @@
+﻿#include "ShockWaveManager.h"
+#include "../../Library/Library.h"
+namespace Game
+{
+	ShockWaveManager::ShockWaveManager()
+	{
+		TEngine::Library::LoadModel("bullet", "Res/NormalSphere.mv1");
+	}
+
+	ShockWaveManager::~ShockWaveManager()
+	{
+		for (auto& shockWave : shockWaves)
+		{
+			delete shockWave;
+		}
+		TEngine::Library::DeleteModel("bullet");
+	}
+
+	void ShockWaveManager::CreateShockWave(ObjBase* obj_)
+	{
+		shockWaves.push_back(new ShockWave(obj_));
+	}
+
+	void ShockWaveManager::Update()
+	{
+		// 弾の更新処理
+		for (auto bullet : shockWaves)
+		{
+			bullet->Update();
+		}
+
+		// 弾の消える処理
+		for (auto itr = shockWaves.begin(), itr_end = shockWaves.end(); itr != shockWaves.end();)
+		{
+			if ((*itr)->IsDead())
+			{
+				delete* itr;
+				itr = bullets.erase(itr);
+			}
+			else
+			{
+				++itr;
+			}
+		}
+	}
+
+	void ShockWaveManager::Draw()
+	{
+		for (auto bullet : shockWaves)
+		{
+			bullet->Draw();
+		}
+	}
+}
---
+++ 3DGameShooting/3DGameShooting/Src/Scene/Game/ShockWaveManager.h
@@ -0,0 +1,32 @@
+﻿#ifndef BULLET_MANAGER_H_
+#define BULLET_MANAGER_H_
+
+#include "Bullet.h"
+#include <vector>
+
+namespace Game
+{
+	// 弾管理クラス
+	class BulletManager
+	{
+	public:
+		BulletManager();
+		~BulletManager();
+
+	public:
+		// 弾の生成
+		void CreateBullet(ObjBase* obj_);
+		// 更新用
+		void Update();
+		// 描画用
+		void Draw();
+
+		std::vector< Bullet* > GetBullets() { return bullets; }
+
+	private:
+		std::vector<Bullet*> bullets{};
+	};
+}
+
+#endif // !BULLET_MANAGER_H_
+
